5ddd59d30a2381d27ee44de0d351e4e53c5241e0
DANA <dana.yohann@gmail.com>
2022-07-08 18:12:10 -0400

    ex00 loading

--- ex00/TemplateEngine.php
+++ ex00/TemplateEngine.php
@@ -1,7 +1,15 @@
 <?php
     class TemplateEngine{
         public function createFile($fileName, $templateName, $parameters){
-            $file = fopen($fileName, "r");
-
+            $file = fopen($fileName, "w");
+            $template = fopen($templateName, "r");
+            while($line = fgets($template)){
+                if (strpos($line, "{") && strpos($line,"}"))
+                    fputs($file, explode($line, '{')[0]);
+                else
+                    fputs($file, $line);
+            }
+            fclose($template);
+            fclose($file);
         }
     }
--- ex00/index.php
+++ ex00/index.php
@@ -1,3 +1,8 @@
 <?php
     include("TemplateEngine.php");
-    
+    $t = new TemplateEngine();
+    $p = array("name" => "Harry", 
+    "auteur" => "JJK",
+    "description" => "test",
+    "prix" => "100");
+    $t->createFile("test", "book_description.html", $p);
