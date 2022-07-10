e2790be6798fec1d41ecb3b51374a82aec76b9f3
DANA <dana.yohann@gmail.com>
2022-07-10 11:53:28 -0400

    ex02 loading

---
+++ ex02/HotBeverage.php
@@ -0,0 +1,18 @@
+<?php
+    class HotBeverage{
+        public $name;
+        public $price;
+        public $resitence;
+
+        public function getName(){
+            return $this->name;
+        }
+        
+        public function getPrice(){
+            return $this->price;
+        }
+
+        public function getResistence(){
+            return $this->resistence;
+        }
+    }
---
+++ ex02/Tea.php
@@ -0,0 +1,7 @@
+<?php
+    include("HotBeverage.php");
+    class Tea extends HotBeverage{
+        
+        private $description = "Miam Delicieux";
+        private $comment = "Non enfaite c'est degueu";
+    }
